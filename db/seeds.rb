d944674aab912fc3f391d5492afc149ec0d138cf
Linwood Lassiter <lin1ood@embarqmail.comx>
2017-09-13 08:42:37 -0400

    Seed update to add user -- before Blog as Blog depends on User many to 1

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: 'Admin', password:'Admin')
+
+
 10.times do
   Blog.create(
     author: Faker::Name.name,
-    subject: Faker::Lorem.paragraph,
+    subject: Faker::Hacker.adjective,
     content: Faker::Hacker.phrases,
     user_id: 1
   )
